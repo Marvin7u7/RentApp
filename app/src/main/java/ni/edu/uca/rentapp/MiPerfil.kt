6758d23429f6dc8abe480621a78e72307a25577d
Emiliano Urbina <emiliano.urbina12171@est.uca.edu.ni>
2021-11-23 16:52:46 -0600

    view profile done

--- app/src/main/java/ni/edu/uca/rentapp/MiPerfil.kt
+++ app/src/main/java/ni/edu/uca/rentapp/MiPerfil.kt
@@ -6,29 +6,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import com.google.android.material.navigation.NavigationView
+import ni.edu.uca.rentapp.EntidadesFrontend.usuarioS
 import ni.edu.uca.rentapp.databinding.ActivityMarvinBinding
+import ni.edu.uca.rentapp.databinding.MiPerfilFragmentBinding
 import ni.edu.uca.rentapp.databinding.NavHeaderArrendatarioyarrendadorBinding
+import ni.edu.uca.rentapp.databinding.RegistrarUsuarioFragmentBinding
 
 class MiPerfil : Fragment() {
+    private lateinit var viewModel: MiPerfilViewModel
+    private lateinit var binding: MiPerfilFragmentBinding
+
 
     companion object {
         fun newInstance() = MiPerfil()
     }
 
-    private lateinit var viewModel: MiPerfilViewModel
-    private lateinit var binding: MiPerfil
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.mi_perfil_fragment, container, false)
+        binding = MiPerfilFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MiPerfilViewModel::class.java)
+        binding.imgPFP.setImageURI(usuarioS.fotoPerfil.toUri())
+        binding.tvNombrePH.text = usuarioS.nombre
+        binding.tvApellidoPH.text = usuarioS.apellido
+        binding.tvEmailPH.text = usuarioS.correo
+        binding.tvMovilPH.text = usuarioS.movil
+        binding.tvCedulaPH.text = usuarioS.cedula
     }
 
 }
