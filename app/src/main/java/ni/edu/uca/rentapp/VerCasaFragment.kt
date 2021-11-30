9bcf10ca5f9ba66abf4f7e119c6a37a6ad530dea
Emiliano Urbina <emiliano.urbina12171@est.uca.edu.ni>
2021-11-30 10:43:20 -0600

    Ver casa lista

--- app/src/main/java/ni/edu/uca/rentapp/VerCasaFragment.kt
+++ app/src/main/java/ni/edu/uca/rentapp/VerCasaFragment.kt
@@ -1,12 +1,18 @@
 package ni.edu.uca.rentapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ni.edu.uca.rentapp.Entidades.Casa
 import ni.edu.uca.rentapp.Interfaces.BuscarCasa.buscar_casaArgs
 import ni.edu.uca.rentapp.databinding.VerCasaFragmentBinding
 
@@ -45,11 +51,28 @@ class VerCasaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val argument: buscar_casaArgs by navArgs()
-        val clickedCasa = viewModel.getCasaById(argument.casaId)
+        //val clickedCasa = viewModel.getCasaById(argument.casaId)
 
-        with(binding) {
-            tvDescripcion.text = clickedCasa.descripcionB
+        lifecycleScope.launch(){
+            withContext(Dispatchers.IO){
+                var clickCasa = viewModel.getCasaById(argument.casaId)
+                try {
+                    if(clickCasa.idCasa != 0){
+                        lifecycleScope.launch(){
+                            withContext(Dispatchers.Main){
+                                with(binding) {
+                                    tvDescripcion.text = clickCasa.descripcionB
+                                }
+                            }
+                        }
+                    }
+                }catch (ex: Exception){
+                    Log.e("ERROR", ex.toString())
+                }
+            }
         }
+
+
     }
 
 //    override fun onActivityCreated(savedInstanceState: Bundle?) {
